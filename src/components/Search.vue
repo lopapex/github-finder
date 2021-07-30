d9251c8e0ebf01b0ec6225d4cce60f142b307c62
Matěj Novotný <485457@muni.cz>
2021-07-30 22:47:14 +0200

    refactoring: extracting search to component

---
+++ src/components/Search.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="search">
+    <b-input-group class="p-5">
+      <template #prepend>
+        <b-input-group-text class="prepend-text bg-primary text-light"
+          >@</b-input-group-text
+        >
+      </template>
+      <b-form-input
+        :placeholder="$t('search.username')"
+        v-model="searchText"
+      ></b-form-input>
+    </b-input-group>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Search",
+
+  computed: {
+    ...mapGetters(["getSearchText"]),
+    searchText: {
+      get() {
+        return this.getSearchText;
+      },
+      set(newValue) {
+        this.downloadUsers(newValue);
+      },
+    },
+  },
+  methods: {
+    ...mapActions(["downloadUsers"]),
+  },
+};
+</script>
+
+<style scope>
+.prepend-text {
+  border: none;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,44 +1,17 @@
 <template>
   <div class="home">
-    <b-input-group class="p-5">
-      <template #prepend>
-        <b-input-group-text class="prepend-text bg-primary text-light"
-          >@</b-input-group-text
-        >
-      </template>
-      <b-form-input
-        :placeholder="$t('home.username')"
-        v-model="searchText"
-      ></b-form-input>
-    </b-input-group>
+    <Search />
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import Search from "../components/Search.vue";
 
 export default {
   name: "Home",
-
-  computed: {
-    ...mapGetters(["getSearchText"]),
-    searchText: {
-      get() {
-        return this.getSearchText;
-      },
-      set(newValue) {
-        this.downloadUsers(newValue);
-      },
-    },
-  },
-  methods: {
-    ...mapActions(["downloadUsers"]),
+  components: {
+    Search,
   },
 };
 </script>
 
-<style scope>
-.prepend-text {
-  border: none;
-}
-</style>
