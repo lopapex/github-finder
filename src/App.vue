755ec4526ea5fc5b8047dbd2e3754b8c3018a9d1
Matěj Novotný <485457@muni.cz>
2021-08-04 14:46:19 +0200

    refactoring: moved global styles to app.vue

--- src/App.vue
+++ src/App.vue
@@ -23,6 +23,37 @@ export default {
   text-align: center;
 }
 
+.hidden_header {
+  display: none;
+}
+
+.page-item .page-link {
+  color: var(--light);
+}
+
+.page-item .page-link {
+  background-color: var(--primary);
+  border-color: var(--primary);
+}
+
+.page-item.disabled .page-link {
+  background-color: var(--primary);
+  border-color: var(--primary);
+  color: var(--light);
+}
+
+.table-hover tbody tr:hover td,
+.table-hover tbody tr:hover th {
+  background-color: var(--light);
+  cursor: pointer;
+}
+
+.table-hover tbody tr td,
+.table-hover tbody tr th {
+  background-color: var(--primary);
+  border: 1px solid var(--secondary);
+}
+
 .content {
   max-width: 1228px;
   margin: auto;
--- src/components/Repositories.vue
+++ src/components/Repositories.vue
@@ -117,36 +117,3 @@ export default {
   },
 };
 </script>
-
-<style>
-.hidden_header {
-  display: none;
-}
-
-.page-item .page-link {
-  color: var(--light);
-}
-
-.page-item .page-link {
-  background-color: var(--primary);
-  border-color: var(--primary);
-}
-
-.page-item.disabled .page-link {
-  background-color: var(--primary);
-  border-color: var(--primary);
-  color: var(--light);
-}
-
-.table-hover tbody tr:hover td,
-.table-hover tbody tr:hover th {
-  background-color: var(--light);
-  cursor: pointer;
-}
-
-.table-hover tbody tr td,
-.table-hover tbody tr th {
-  background-color: var(--primary);
-  border: 1px solid var(--secondary);
-}
-</style>
